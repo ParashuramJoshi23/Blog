90170cb2957777074e6635b1ed350987f29dfce9
ParashuramJoshi23 <parashuramjoshi23@gmail.com>
2018-02-21 12:06:15 +0530

    Added_Comment_Models

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 # Intialise Admin for model Post
 
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'parashuram',
+        'PASSWORD': 'Password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
